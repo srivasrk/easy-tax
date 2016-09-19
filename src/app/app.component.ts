54cadeeced791401749f0613375e5f195af13123
Rahul Srivastava <srivasrk@mail.uc.edu>
2016-09-18 22:43:33 -0400

    'update-tml-next-prev-logic'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-// import { HeaderComponent } from "./header/header.component";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'et-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['../app/css/global.css', '../app/css/social-media.css']
+  // styleUrls: ['css/global.css', 'css/social-media.css']
 })
 export class AppComponent {
   appBgPhoto : string  = 'https://www.betterment.com/wp-content/uploads/2013/04/piggybank.jpg';
--- src/app/testimonials/testimonials-list/testimonials-list.component.ts
+++ src/app/testimonials/testimonials-list/testimonials-list.component.ts
@@ -10,7 +10,8 @@ import { TestimonialService } from "../testimonial.service";
 
 export class TestimonialsListComponent implements OnInit {
   testimonials: Testimonial[] = [];
-  activeTmlId: number = 0;
+  nextTmlId: number = 0;
+  previousTmlId: number = 0;
 
   constructor(private testimonialService: TestimonialService) { }
 
@@ -21,29 +22,52 @@ export class TestimonialsListComponent implements OnInit {
     );
   }
 
+  //prev/next buttons (div) is shown on basic assumption is that you have minimum of 3 testimonials
   previousTestimonial(){
-    if (this.activeTmlId == 0){
-      this.activeTmlId = this.testimonials.length - 1;//circle back to the last testimonial
-
+    if (this.previousTmlId == 0){
+      this.previousTmlId = this.testimonials.length - 1;//circle back to the last testimonial
     }
-    else if (this.activeTmlId > 0) {
-      this.activeTmlId = this.activeTmlId - 1;
+    else if (this.previousTmlId > 0) {
+      this.previousTmlId = this.previousTmlId - 1;
+    }
+
+    //update the next Tml Id also
+    if (this.previousTmlId > this.testimonials.length - 3){
+      this.nextTmlId = 0;
+    }
+    else if (this.previousTmlId > this.testimonials.length - 2){
+      this.nextTmlId = 1;
+    }
+    else {
+      this.nextTmlId = this.previousTmlId + 2;
     }
   }
 
+  //prev/next buttons (div) is shown on basic assumption is that you have minimum of 3 testimonials
   nextTestimonial(){
-    if (this.activeTmlId == this.testimonials.length - 1){
-      this.activeTmlId = 0;//circle back to the first testimonial
-
+    if (this.nextTmlId == this.testimonials.length - 1){
+      this.nextTmlId = 0;//circle back to the first testimonial
     }
-    else if (this.activeTmlId < this.testimonials.length - 1) {
-      this.activeTmlId = this.activeTmlId + 1;
+    else if (this.nextTmlId < this.testimonials.length - 1) {
+      this.nextTmlId = this.nextTmlId + 1;
+    }
+
+    //update the previous Tml Id also
+    if (this.nextTmlId < 1){
+      this.previousTmlId = this.testimonials.length - 2;
+    }
+    else if (this.nextTmlId < 2){
+      this.previousTmlId = this.testimonials.length - 1;
+    }
+    else {
+      this.previousTmlId = this.nextTmlId - 2;
     }
   }
 
   activeTmlIdChanged(tmlId: number){
     if (tmlId != null) {
-      this.activeTmlId = tmlId;
+      this.previousTmlId = tmlId;
+      this.nextTmlId = tmlId;
       this.nextTestimonial();//to already increment the activeTmlId once because the click event is triggered after the
                               // activeTmlId has been accessed by the html
     }
