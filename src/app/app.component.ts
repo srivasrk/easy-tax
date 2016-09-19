eeef757ee6486b33e99bca8a5a4f5beeb315cf05
Rahul Srivastava <srivasrk@mail.uc.edu>
2016-09-18 20:00:57 -0400

    'update-css'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'et-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['/css/global.css']
-  // directives: [ HeaderComponent ]
+  styleUrls: ['../app/css/global.css']
 })
 export class AppComponent {
   appBgPhoto : string  = 'https://www.betterment.com/wp-content/uploads/2013/04/piggybank.jpg';
