95cbdb5ab405d8ba966c13c513dd124f3d5abeca
Rahul Srivastava <srivasrk@mail.uc.edu>
2016-09-19 20:57:22 -0400

    'ready'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'et-root',
-  templateUrl: 'app.component.html',
-  // styleUrls: ['css/global.css', 'css/social-media.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   appBgPhoto : string  = 'https://www.betterment.com/wp-content/uploads/2013/04/piggybank.jpg';
